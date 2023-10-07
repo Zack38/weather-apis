85b1f352297e7d76fb0ae8fca5f70415e0c7057d
Zack Lash <zlash8333@gmail.com>
2023-10-07 11:57:46 -0400

    Fixed the container name in docker compose & pytest files

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -8,7 +8,7 @@ client = TestClient(app)
 
 print(client)
 # Define the hostname of the linked API container
-api_container_hostname = "weather-apis"
+api_container_hostname = "gateway-apis"
 
 def test_read_root_v1():
     response = client.get(f"http://{api_container_hostname}:8000/v1/")
@@ -18,24 +18,24 @@ def test_read_root_v1():
 def test_get_weather():
     response = client.get(f"http://{api_container_hostname}:8000/v1/weather")
     assert response.status_code == 200
-    # Add more specific assertions based on your expected data format
+    # specific assertions based on your expected data format
 
 def test_get_weather_limit():
     response = client.get(f"http://{api_container_hostname}:8000/v1/weather-limit?limit=5")
     assert response.status_code == 200
-    # Add more specific assertions based on your expected data format
+    # specific assertions based on your expected data format
 
 def test_get_weather_by_date():
     response = client.get(f"http://{api_container_hostname}:8000/v1/weather-by-date?date=2022-06-04")
     assert response.status_code == 200
-    # Add more specific assertions based on your expected data format
+    # specific assertions based on your expected data format
 
 def test_get_weather_by_type():
     response = client.get(f"http://{api_container_hostname}:8000/v1/weather-by-type?weather=rain")
     assert response.status_code == 200
-    # Add more specific assertions based on your expected data format
+    # specific assertions based on your expected data format
 
 def test_get_weather_multi():
     response = client.get(f"http://{api_container_hostname}:8000/v1/weather-multi?limit=5&date=2022-06-04&weather=rain")
     assert response.status_code == 200
-    # Add more specific assertions based on your expected data format
+    # specific assertions based on your expected data format
